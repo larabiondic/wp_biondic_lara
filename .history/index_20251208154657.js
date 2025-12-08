e8fa593f16d75eced41ef2b5206acdf2d78ae569
larabiondic <larabiondic51@gmail.com>
2025-12-08 16:04:00 +0100

    js, grid

---
+++ .history/index_20251208154657.js
@@ -0,0 +1,14 @@
+let broj1 = prompt ("Unesi prvi broj");
+let broj2 = prompt ("Unesi drugi broj");
+if(broj1 > broj2)
+{
+    alert (broj1 + "je veći od" + broj2);
+}
+else if(broj1 < broj2)
+{
+    alert (broj2 + "je veći od" + broj1);
+}
+else 
+{
+    alert ("Brojevi su jednaki");
+}
---
+++ .history/index_20251208160135.js
@@ -0,0 +1,28 @@
+/* let broj1 = prompt ("Unesi prvi broj");
+let broj2 = prompt ("Unesi drugi broj");
+if(broj1 > broj2)
+{
+    alert (broj1 + "je veći od" + broj2);
+}
+else if(broj1 < broj2)
+{
+    alert (broj2 + "je veći od" + broj1);
+}
+else 
+{
+    alert ("Brojevi su jednaki");
+} */
+
+    /* BMI App 
+
+        BMI = masa (kg) / visina*visina (m)
+        -unijeti masu i visinu 
+        - ispisati bmi (vaš BMI iznosti: xx )
+        ) */
+
+
+let tezina = prompt("Unesi svoju težinu u kg:");
+let visina = prompt("Unesi svoju visinu u metrima:");
+let bmi = Math.round(bmi);(tezina / (visina * visina));
+
+alert("Vaš BMI iznosi: " + bmi);
